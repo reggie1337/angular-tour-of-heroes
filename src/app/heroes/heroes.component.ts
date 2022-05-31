4248a2455b91109a22c3752e7feb8e5105883081
reggie <reggie.skillman34@icloud.com>
2022-05-31 18:08:16 -0400

    stlyingAdded

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from './hero';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heros';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+//exposed heroes array for binding
+  heroes = HEROES;
 //refactored hero property to be of type Hero
   hero: Hero = {
     id : 1,

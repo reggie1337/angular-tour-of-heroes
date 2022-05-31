60b538991f84efdc17328c66e0062c9a124907ff
reggie <reggie.skillman34@icloud.com>
2022-05-31 17:10:15 -0400

    refactorHeroPropertytoHeroObj

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Hero } from './hero';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+//refactored hero property to be of type Hero
+  hero: Hero = {
+    id : 1,
+    name : "Windstorm"
+  };
 
   constructor() { }
 
   ngOnInit(): void {
   }
-  hero = 'Windstorm';
+
 
 }

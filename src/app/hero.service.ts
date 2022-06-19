f92c88208ad9671ad027b7aeaa7b3932890e5cef
reggie <reggie.skillman34@icloud.com>
2022-06-18 22:12:31 -0400

    deleteAdded

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,25 +1,60 @@
 import { Injectable } from '@angular/core';
 import { HEROES } from "./mock-heros";
 import {Hero} from "./hero";
-import { Observable, of } from 'rxjs';
+import { from, Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { catchError,map,tap } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class HeroService {
-  //returns Observable of array of mock heroes
-  getHeroes():Observable< Hero []>{
-    const heroes = of(HEROES)
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+
+  private heroesUrl = 'api/heroes';
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
+  private handleError<T>(operation = 'opperation', result? : T){
+    return (error:any): Observable<T> => 
+    {console.error(error); 
+    this.log(`${operation} failed: ${error.message}`)
+    return of (result as T);
+    }
+    
+  }
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+//calls hero [] from a remote server
+  getHeroes():Observable< Hero []>{
+    return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      tap(_=> this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes',[])))
+  };
   //find requires predicate
   getHero(id: number): Observable<Hero>{
-    const hero = HEROES.find(h => h.id ===id)!;
-    this.messageService.add(`HeroService: fetched hero id = ${id}`);
-    return of (hero);
-  }
-  constructor(private messageService: MessageService) { }
-}
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`)))
+    }
+    updateHero(hero: Hero): Observable <any> {
+      return this.http.put(this.heroesUrl, hero,this.httpOptions)
+      .pipe( tap(_=>this.log(`updated hero id= ${hero.id}`)),
+      catchError(this.handleError<any>('updateHero')));
+    }
 
+    httpOptions = {
+      headers: new HttpHeaders ({'Content-Type' : 'application/json'})
+    }
+    addHero(hero:Hero) : Observable<Hero> {
+      return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+        tap((newHero : Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+        catchError(this.handleError<Hero>('addHero')));
+    }
+
+} 
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -14,7 +14,7 @@ export class HeroesComponent implements OnInit {
 //refactored hero property to be of type Hero
   hero: Hero = {
     id : 1,
-    name : "Windstorm"
+    name : "Windstorm",
   };
 
   constructor(private heroService : HeroService, private messageService: MessageService) { }
@@ -30,6 +30,16 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
-
-
+  add(name:string){
+    name = name.trim();
+    if(!name) {return; }
+    this.heroService.updateHero({ name } as Hero).subscribe(hero =>{
+      this.heroes.push(hero)
+    })
+  }
+  delete(heroes:Hero) {
+    const index = this.heroes.findIndex((h)=>h.id ===heroes.id)
+    this.heroes.splice(index,1)
+  }
+  
 }

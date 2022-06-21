0f596ce0ff97ea3b13553c25e5fd24172fec8d63
reggie <reggie.skillman34@icloud.com>
2022-06-21 12:51:56 -0400

    searchComponentCreated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,16 @@ import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http'
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api'
-import { InMemoryDataService } from './in-memory-data.service'
+import { InMemoryDataService } from './in-memory-data.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component'
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/hero-search/hero-search.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime,distinctUntilChanged,switchMap } from 'rxjs';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+@Component({
+  selector: 'app-hero-search',
+  templateUrl: './hero-search.component.html',
+  styleUrls: ['./hero-search.component.css']
+})
+export class HeroSearchComponent implements OnInit {
+  heroes$!: Observable<Hero[]>;
+  private searchTerms = new Subject <string>();
+
+
+  constructor(private heroService: HeroService) { }
+
+  //search term introd into observable stream
+  search(term: string) {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit(): void {
+    this.heroes$ = this.searchTerms.pipe(
+      //will wait 300ms before considering the term
+      debounceTime (300),
+
+      //ignores new term if same as previous
+      distinctUntilChanged(),
+      //switch to new search observable when term changes
+      switchMap((term: string) => this.heroService.searchHeroes(term))
+    );
+  }
+
+}

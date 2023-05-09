0ecc32b53d828aeb06b0d54f8b47235956a4bb63
reggie <reggie.skillman34@icloud.com>
2023-05-09 17:11:09 -0400

    backend created along with a potential connection to db

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,30 +1,23 @@
 import { Injectable } from '@angular/core';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { Hero } from './hero';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 //create the database of heros
 export class InMemoryDataService implements InMemoryDbService {
-  createDb(){
-    const heroes = [
-      { id: 12, name: 'Dr.Nice'},
-      { id: 13, name: 'Bombasto'},
-      { id: 14, name: 'Celeritas'},
-      { id: 15, name: 'Magneta'},
-      { id: 16, name: 'Rubberman'},
-      { id: 17, name: 'Dynama'},
-      { id: 18, name: 'Dr.IQ'},
-      { id: 19, name: 'Magma'},
-      { id: 20, name: 'Tornado'}
-    ];
-    return {heroes};
+  private heroesUrl = 'http://localhost:3000/heroes';
+
+  constructor(private http: HttpClient) {}
+  createDb(): Observable<{ heroes: Hero[] }> {
+    return this.http.get<{ heroes: Hero[] }>(this.heroesUrl);
   }
   genId(heroes: Hero[]): number {
-    return heroes.length >0 ? 
-    Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+    return heroes.length > 0
+      ? Math.max(...heroes.map((hero) => hero.id)) + 1
+      : 11;
   }
-
-  constructor() { }
 }
